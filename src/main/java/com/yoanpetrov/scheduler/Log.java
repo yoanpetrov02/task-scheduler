751f51adb986e1ac85489d35296b7c6f3c881637
yoanpetrov02 <yoanpetrov0230@gmail.com>
2025-04-15 16:16:21 +0300

    Logging setup.

---
+++ src/main/java/com/yoanpetrov/scheduler/Log.java
@@ -0,0 +1,9 @@
+package com.yoanpetrov.scheduler;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public final class Log {
+
+    public static final Logger logger = LogManager.getLogger("Task Scheduler");
+}
---
+++ src/main/java/com/yoanpetrov/scheduler/Main.java
@@ -0,0 +1,8 @@
+package com.yoanpetrov.scheduler;
+
+public class Main {
+
+    public static void main(String[] args){
+        Log.logger.info("sample message lalaallala");
+    }
+}
